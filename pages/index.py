7c9aa7fac1907b39c46c39e6134acb5b5f169e54
csslambda <sisi.chen@lambdaschool.com>
2021-03-03 11:17:13 -0800

    change the import order

--- pages/index.py
+++ pages/index.py
@@ -4,9 +4,9 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
 import plotly.express as px
 
-import pandas as pd
 
 # Imports from this application
 from app import app
